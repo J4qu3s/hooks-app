374ce3def337d0dc0bbfede9547f20b930546da5
j4qu3s <jarkko.hoyhtya@gmail.com>
2022-05-02 11:55:17 +0300

    minor fixes

--- src/components/Search.js
+++ src/components/Search.js
@@ -22,22 +22,26 @@ const Search = () => {
 
             setResults(data.query.search);
 
-        }
-        if(term){
-            search();
-        }
-        
-        
+        };
+        const timeoutId = setTimeout(() => {
+            if(term){
+                search();
+            }
+        }, 500);       
     }, [term]);
 
     const renderedResults = results.map((result) => {
         return (
             <div className="item" key={result.pageid}>
+                <div className="right floated content">
+                    <a href={`https://en.wikipedia.org?curid=${result.pageid}`} 
+                    className="ui button">Go</a>
+                </div>
                 <div className="content">
                     <div className="header">
                         {result.title}
                     </div>
-                    {result.snippet}
+                    <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
                 </div>
             </div>
         )
