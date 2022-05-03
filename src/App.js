ed85eb47743a06764f16b3133834291967a663d7
j4qu3s <jarkko.hoyhtya@gmail.com>
2022-05-03 18:14:30 +0300

    On hooks dropdown

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
 import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
 import Search from './components/Search';
+
+
+
+const items = [
+  {
+    title: "what is react??",
+    content: "Something that you do when you get surprised!"
+  },
+  {
+    title: "what is react??",
+    content: "Something that you do when you get surprised!"
+  },
+  {
+    title: "what is react??",
+    content: "Something that you do when you get surprised!"
+  }
+];
+
+const options = [
+  {
+    label: 'The color Red',
+    value: 'red'
+  },
+  {
+    label: 'The color Yellow',
+    value: 'yellow'
+  },
+  {
+    label: 'The color Blue',
+    value: 'blue'
+  }
+]
 
 export default () => {
 
-  const items = [
-    {
-      title : "what is react??",
-      content : "Something that you do when you get surprised!"
-    },
-    {
-      title : "what is react??",
-      content : "Something that you do when you get surprised!"
-    },
-    {
-      title : "what is react??",
-      content : "Something that you do when you get surprised!"
-    }
-  ]
+  const [selected, setSelected] = useState(options[0]);
 
   return (
     <div>
-      <Search/>
+      <Dropdown 
+      options={options} 
+      selected={selected}
+      onSelectedChange={setSelected}
+      />
     </div>
-  )
-}
+  );
+};
